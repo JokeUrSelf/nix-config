9ea7023e6732777371730a29b15cded3bbdd97c1
Mykyta Mykulskyi <my.garbage813@gmail.com>
2025-02-06 17:31:57 +0100

    init

---
+++ configuration.nix
@@ -0,0 +1,119 @@
+{ config, pkgs, ... }:
+
+let
+  nix-software-center = import (
+    pkgs.fetchFromGitHub {
+      owner = "snowfallorg";
+      repo = "nix-software-center";
+      rev = "0.1.2";
+      sha256 = "xiqF1mP8wFubdsAQ1BmfjzCgOD3YZf7EGWl9i69FTls=";
+    }
+  ) {};
+in
+{
+  imports = [ ./hardware-configuration.nix ./nvidia-configuration.nix ];
+
+  boot.loader = { 
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Warsaw";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
+
+  services.xserver = {
+    enable = true;
+
+    videoDrivers = [ "nvidia" ];
+    desktopManager.xterm.enable = false;
+
+    displayManager.lightdm.enable = true;
+    windowManager.i3.enable = true;
+    # desktopManager.gnome.enable = true;
+    
+    xkb = {
+      layout = "us";
+      variant = "colemak";
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
+  };
+
+  services.printing.enable = true;
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mik = {
+    isNormalUser = true;
+    description = "Mik";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config = { 
+    allowUnfree = true;
+    nvidia.acceptLicense = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    vscode
+    jetbrains.pycharm-community
+
+    gcc
+    git
+    gnumake
+    htop
+    fzf
+    zip
+    unzip
+
+    ghostty
+    nix-software-center
+    google-chrome
+    telegram-desktop
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # services.openssh.enable = true;
+
+  system.stateVersion = "24.11";
+}
---
+++ nvidia-configuration.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+
+    prime = {
+      offload = { enable = true; enableOffloadCmd = true; };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+}
